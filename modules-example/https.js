9533d394b7fcdd64d933f7131cbc56fc635df2e8
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-01 11:08:19 +0300

    using folder as a modeule we can use folder as a module with index.js in it and require the folder itself. index.js exports all necessery utilities

--- modules-example/https.js
+++ modules-example/https.js
@@ -1,13 +1,14 @@
-const { send } = require("./request.js");
-const { read } = require("./response.js");
+const internals = require("./internals");
+// const { send } = require("./request.js");
+// const { read } = require("./response.js");
 
 function request(url, data) {
-  const resonse = send(url, data);
-  return read(resonse);
+  const resonse = internals.request.send(url, data);
+  return internals.response.read(resonse);
 }
 
 const req = request("https://www.google.com/", "Brendan Eich");
 
-console.log("[3]",req);
+console.log("[3]", req);
 
-module.children.map(m => console.log(m.exports))
+module.children.map((m) => console.log(m.exports));
