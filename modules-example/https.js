06c69ae4b96c99c3de8ae6d6649081fac2b113e3
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-01 11:24:57 +0300

    using index and folder as a module is not good using folder as module add extra logic while importing it. Overal it is not recommended but knowing it is a good, help to understand when we encounter this kind of importing modules

--- modules-example/https.js
+++ modules-example/https.js
@@ -1,14 +1,13 @@
-const internals = require("./internals");
-// const { send } = require("./request.js");
-// const { read } = require("./response.js");
+const { send } = require("./request.js");
+const { read } = require("./response.js");
 
 function request(url, data) {
-  const resonse = internals.request.send(url, data);
-  return internals.response.read(resonse);
+  const resonse = send(url, data);
+  return read(resonse);
 }
 
 const req = request("https://www.google.com/", "Brendan Eich");
 
-console.log("[3]", req);
+console.log("[3]",req);
 
-module.children.map((m) => console.log(m.exports));
+module.children.map(m => console.log(m.exports))
