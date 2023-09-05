a9550ae312ec240925d0e1b68e11e24509dc4c6a
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-05 09:39:55 +0300

    handling api end points

--- http-server/index.js
+++ http-server/index.js
@@ -4,12 +4,55 @@ const PORT = 7777;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+];
+
 server.on("request", (req, res) => {
-  console.log(res);
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
-  res.end("hello, there");
+  const items = req.url.split("/");
+
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request:", friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === "GET" && items[1] === "friends") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "message") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Esaac!</li>");
+    res.write("<li>What are you thoughts on astranomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, console.log("working"));
